37a1fb0d27c27ab550b848b7bb299b64078d4447
ZZW <1594870836@qq.com>
2019-12-11 21:00:35 +0800

    jwt验证

--- LaoZhangCoreTwo/Controllers/ValuesController.cs
+++ LaoZhangCoreTwo/Controllers/ValuesController.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Remotion.Linq.Clauses.Expressions;
 
 namespace LaoZhangCoreTwo.Controllers
 {
@@ -10,18 +17,59 @@ namespace LaoZhangCoreTwo.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly IHttpContextAccessor _accessor;
+
+        public ValuesController(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "laozhang"),
+                new Claim(JwtRegisteredClaimNames.Email, "123456@qq.com"),
+                new Claim(JwtRegisteredClaimNames.Sub, "1"),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("laozhanglaozhang"));
+
+            var token=new JwtSecurityToken(
+                issuer:"http://localhost:5000",
+                audience:"http://localhost:5001",
+                claims:claims,
+                expires:DateTime.Now.AddHours(1),
+                signingCredentials:new SigningCredentials(key,SecurityAlgorithms.HmacSha256)
+                );
+
+            var jwtToken=new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new string[] { jwtToken };
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [HttpGet("{jwtStr}")]
+        public ActionResult<IEnumerable<string>> Get(string jwtStr)
         {
-            return "value";
+//            var jwtHandler=new JwtSecurityTokenHandler();
+//            JwtSecurityToken jwtToken = jwtHandler.ReadJwtToken(jwtStr);
+//
+//            var sub = User.FindFirst(d => d.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
+
+            var name = _accessor.HttpContext.User.Identity.Name;
+            var claims = _accessor.HttpContext.User.Claims;
+            var claimTypeVal = (from claim in claims
+                where claim.Type == JwtRegisteredClaimNames.Email
+                select claim.Value).ToList();
+
+            return new string[]
+            {
+                name,
+                JsonConvert.SerializeObject(claimTypeVal)
+            };
+
         }
 
         // POST api/values
--- LaoZhangCoreTwo/Startup.cs
+++ LaoZhangCoreTwo/Startup.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LaoZhangCoreTwo
 {
@@ -24,7 +27,30 @@ namespace LaoZhangCoreTwo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var symmetricKeyAsBase64 = "laozhanglaozhang";
+            var keyByteArray = Encoding.ASCII.GetBytes(symmetricKeyAsBase64);
+            var signingKey = new SymmetricSecurityKey(keyByteArray);
+
+            var tokenValidationParameters=new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = "http://localhost:5000",
+                ValidateAudience = true,
+                ValidAudience = "http://localhost:5001",
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.FromSeconds(30),
+                RequireExpirationTime = true
+            };
+
+            services.AddAuthentication("Bearer").AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = tokenValidationParameters;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +61,7 @@ namespace LaoZhangCoreTwo
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
